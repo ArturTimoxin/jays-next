1947020eb6cbd7fbeb400ffce4a5c0fcb50f2791
ArturTimoxin <timoxin.artur@gmail.com>
2020-05-06 19:10:41 +0300

    add fb pixel code

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -33,6 +33,23 @@ const Layout = ({
 
           gtag('config', 'UA-159338082-1');
         `}} />
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+           fbq('init', '647362729328538'); 
+          fbq('track', 'PageView');
+        `}} />
+        <noscript>
+        <img height="1" width="1" 
+        src="https://www.facebook.com/tr?id=647362729328538&ev=PageView
+        &noscript=1"/>
+        </noscript>
         <link
           href="https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700&display=swap&subset=cyrillic"
           rel="stylesheet"
@@ -44,6 +61,7 @@ const Layout = ({
           crossOrigin="anonymous"
         />
         <script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}`}></script>
+        
       </Head>
       <div className="wrap-page">
         <Header />
